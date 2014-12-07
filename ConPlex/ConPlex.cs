c511aed8056aed33219d56d27b57da7614e3b5b0
matth0x1 <matth0x1@gmail.com>
2014-12-07 12:18:40 +0000

    Begun prototyping class structure.

---
+++ ConPlex/ConPlex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConPlex
+{
+	public static class ConPlex
+	{
+		public static ConPlexConsoleCollection Consoles = new ConPlexConsoleCollection ();
+	}
+
+	public class ConPlexConsoleCollection
+	{
+		private readonly object ConsolesLocker = new object();
+		private Dictionary<string, ConPlexConsole> Consoles = new Dictionary<string, ConPlexConsole>(StringComparer.OrdinalIgnoreCase);
+
+		public ConPlexConsole this [string consoleName]
+		{
+			get
+			{
+				ConPlexConsole console;
+
+				if(!Consoles.TryGetValue (consoleName, out console))
+					lock (ConsolesLocker) 
+						if (!Consoles.TryGetValue (consoleName, out console))
+						{
+							console = new ConPlexConsole (consoleName);
+							Consoles.Add(consoleName, console);
+						}
+
+				return console;
+			}
+		}
+	}
+
+	public class ConPlexConsole
+	{
+		public ConPlexConsole (string name)
+		{
+			Name = name;
+		}
+
+		public string Name {
+			get;
+			private set;
+		}
+
+		public void Write(string value )
+		{
+			Console.WriteLine (Name + ": " + value);
+		}
+	}
+}
+
---
+++ ConPlexTestHandle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConPlex;
+
+namespace ConPlexTestHandle
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("ConPlex TestHandle");
+			Console.Write ("\n" + new string ('_', Console.BufferWidth));
+
+
+			ConPlex.ConPlex.Consoles ["Debug 1"].Write ("Hollow World.");
+			ConPlex.ConPlex.Consoles ["Debug 2"].Write ("Hollow World.");
+			ConPlex.ConPlex.Consoles ["Debug 3"].Write ("Hollow World.");
+			ConPlex.ConPlex.Consoles ["Debug 1"].Write ("Hollow World.");
+			ConPlex.ConPlex.Consoles ["Debug 3"].Write ("Hollow World.");
+
+			Console.ReadLine();
+		}
+	}
+}
